c9bd0694d69de9a8e1283c716241448b79021783
Aleksander <aleksander.ramut@wp.pl>
2025-09-16 20:22:09 +0200

    Game stage in QML prepared

--- create_container.sh
+++ create_container.sh
@@ -4,4 +4,4 @@
 docker build -t scorched-earth .
 
 # Run the container in the background (detached mode)
-docker run -d --name scorched-earth --net=host -e DISPLAY=$DISPLAY -v $(pwd):$(pwd) -w $(pwd) scorched-earth tail -f /dev/null
+docker run -d --name scorched-earth --device /dev/dri --net=host -e DISPLAY=$DISPLAY -v $(pwd):$(pwd) -w $(pwd) scorched-earth tail -f /dev/null
